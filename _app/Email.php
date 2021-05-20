5d52febeb34aa0394e4a3ab6bf1da33efb3d0cd2
Joao Vitor <joaovitor0072010@gmail.com>
2021-05-20 15:20:37 -0300

    Library OK

--- _app/Email.php
+++ _app/Email.php
@@ -9,20 +9,20 @@ class Email {
 
     private $mail = \stdClass::class;
 
-    public function __construct() {
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName) {
         $this->mail = new PHPMailer(true);
-        $this->mail->SMTPDebug = 2;                      //Enable verbose debug output
-        $this->mail->isSMTP();                                            //Send using SMTP
-        $this->mail->Host = 'smtp.office365.com';                     //Set the SMTP server to send through
-        $this->mail->SMTPAuth = true;                                   //Enable SMTP authentication
-        $this->mail->Username = 'cursojoao@outlook.com';                     //SMTP username
-        $this->mail->Password = 'joao1415';                               //SMTP password
-        $this->mail->SMTPSecure = 'starttls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $this->mail->SMTPDebug = $smtpDebug;               //Enable verbose debug output
+        $this->mail->isSMTP();                             //Send using SMTP
+        $this->mail->Host = $host;                         //Set the SMTP server to send through
+        $this->mail->SMTPAuth = true;                      //Enable SMTP authentication
+        $this->mail->Username = $user;                     //SMTP username
+        $this->mail->Password = $pass;                     //SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;             //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $this->mail->Port = $port;                         //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         $this->mail->CharSet = 'utf-8';
         $this->mail->setLanguage('br');
         $this->mail->isHTML(true);
-        $this->mail->Port = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-        $this->mail->setFrom('cursojoao@outlook.com', 'Equipe Joao');
+        $this->mail->setFrom($setFromEmail, $setFromName);
     }
 
     public function sendMail($subject, $body, $replyEmail, $replayName, $addressEmail, $addressName) {
